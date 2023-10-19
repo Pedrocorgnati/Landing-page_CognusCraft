4101ac02ea0c0b787361afea639d858a4d79a537
Pedro <corgnati.pedro@gmail.com>
2023-10-19 19:30:19 -0300

    teste

---
+++ dist/index.9c66a8ab.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded",function(){let t=document.querySelector(".button__contact-us"),e=document.querySelector(".logo__mail"),n=e.getAttribute("src"),o=e.getAttribute("data-hover-icon");t.addEventListener("mouseenter",function(){e.setAttribute("src",o)}),t.addEventListener("mouseleave",function(){e.setAttribute("src",n)})});//# sourceMappingURL=index.9c66a8ab.js.map
+
+//# sourceMappingURL=index.9c66a8ab.js.map
